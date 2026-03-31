8bc705499140dddd9f77036caf1b25621b28b6e8
hihicong <cong29212@gmail.com>
2026-03-31 09:12:35 +0800

    20260331 remove st-pages

--- ST_intro.py
+++ ST_intro.py
@@ -5,7 +5,6 @@ from io import StringIO
 import pandas as pd
 import toml
 import importlib.util
-from st_pages import add_page_title, get_nav_from_toml
 
 def main():
     st.set_page_config(page_title="AI應用分析", page_icon="✨")
